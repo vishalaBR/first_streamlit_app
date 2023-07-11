0994de5a98cee2a055262de839c8f62aa9865642
VishalaBR321 <139209151+VishalaBR321@users.noreply.github.com>
2023-07-11 14:31:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parenrs new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale ,Pinatch and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range wgg')
+streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale ,Pinatch and Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range wgg')
+streamlit.text('🥑🍞Avacado Toast')
