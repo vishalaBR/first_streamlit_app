fbe71c5fd0cddbc5fc4d56de3e68664a1b42074b
VishalaBR321 <139209151+VishalaBR321@users.noreply.github.com>
2023-07-16 14:43:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range wgg')
 streamlit.text('🥑🍞Avacado Toast')
 
 
-
+import pandas
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
